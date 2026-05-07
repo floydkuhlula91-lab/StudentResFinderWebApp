8b3eb3de92c793f2b4d0289f6c95f7e5649596e3
floydkuhlula91-lab <floydkuhlula91@gmail.com>
2026-05-07 14:54:20 +0200

    Update SearchSB.java

--- src/business/SearchSB.java
+++ src/business/SearchSB.java
@@ -1,4 +1,29 @@
 package business;
 
+import dao.ListingDAO;
+import model.Listing;
+import java.sql.SQLException;
+import java.util.List;
+import javax.ejb.Stateless;
+
+@Stateless
 public class SearchSB {
+    private ListingDAO listingDAO = new ListingDAO();
+
+    public List<Listing> searchByLocation(String location) throws SQLException {
+        return listingDAO.searchByLocation(location);
+    }
+
+    public List<Listing> filterByPrice(double min, double max) throws SQLException {
+        return listingDAO.filterByPrice(min, max);
+    }
+
+    public List<Listing> getAllListings() throws SQLException {
+        return listingDAO.getAllListings();
+    }
+
+    public Listing getListingById(int id) throws SQLException {
+        return listingDAO.getListingById(id);
+    }
 }
+
