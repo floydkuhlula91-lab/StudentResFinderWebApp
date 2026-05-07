842499dcf54c5c648ce8b30d9d9e96ec37257c70
floydkuhlula91-lab <floydkuhlula91@gmail.com>
2026-05-07 15:05:41 +0200

    Update ViewListingDetailsServlet.java

--- src/servlet/ViewListingDetailsServlet.java
+++ src/servlet/ViewListingDetailsServlet.java
@@ -1,4 +1,29 @@
 package servlet;
 
-public class ViewListingDetailsServlet {
+import business.SearchSB;
+import model.Listing;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/viewListing")
+public class ViewListingDetailsServlet extends HttpServlet {
+    private SearchSB searchSB = new SearchSB();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        try {
+            Listing listing = searchSB.getListingById(id);
+            request.setAttribute("listing", listing);
+            RequestDispatcher rd = request.getRequestDispatcher("viewListing.jsp");
+            rd.forward(request, response);
+        } catch (SQLException e) {
+            throw new ServletException("Error retrieving listing details", e);
+        }
+    }
 }
